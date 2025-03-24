cd14fa97469c227afbc8295a32a129143b23e2cc
Rekoyah <bludonut95@gmail.com>
2025-03-24 13:55:09 -0500

    created array for seven names

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ window.addEventListener("load", function() {
         localStorage.setItem("name", person);
     }
 });
+
+let names = [];
+let numberOfNames = 7;
+
+window.addEventListener("load", function() { 
+    for (let i = 0; i < numberOfNames; i++) {
+    let name = prompt(`Enter name ${i + 1}`)
+    if (name !== null) {
+        names.push(name);
+        console.log(names);
+    }
+
+}});
+
